226f7691ad3c60d760690a425ca2818cdf67e3de
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-18 21:53:30 +0800

    AddTwoSums

--- src/AddTwoSums.java
+++ src/AddTwoSums.java
@@ -16,7 +16,7 @@ public class AddTwoSums {
                 left = mid + 1;
             }
             else
-                right = mid + 1;
+                right = mid - 1;
         }
         return false;
     }
